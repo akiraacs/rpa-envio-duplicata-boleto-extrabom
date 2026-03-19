737369f08a4a69486c625d343765b7a88179b281
akiraacs <alfabeto.coradini@hotmail.com>
2026-03-19 14:22:25 -0300

    Adequa main.py para execucao completa do processo

--- main.py
+++ main.py
@@ -5,58 +5,55 @@ from src.apps.consinco_operador_desktop import ConsincoOperadorDesktop
 from src.config.settings import settings
 from src.packages.email import Email
 from src.utils.comandos_cmd import executar_cmds_manter_sessao_ativa, fechar_sistemas_legados
-from src.utils.tratamento_arquivos import limpar_pasta, criar_pastas_essencias_caso_inexistente
 
 
 def main() -> None:
     """Funcao principal"""
     try:
-        nome_rpa = "Envio de Duplicata de Boleto"
+        nome_rpa = "Envio de Duplicatas/Boletos"
         logger.info(f"Iniciando robô - {nome_rpa}")
         execucao_com_erro = False
-        msg_data_tratativa = ""
         app_consinco_operador:ConsincoOperadorDesktop = None
         msg_erro = ""
-
-        # executar_cmds_manter_sessao_ativa()
-        # fechar_sistemas_legados()
-        # criar_pastas_essencias_caso_inexistente()
-        # limpar_pasta(caminho_pasta=settings.caminho.pasta_temp)
-
-        msg_data_tratativa = f"Data considerada para consulta/tratativa: {settings.processo.data_conciliacao}"
+        msg_data_tratativa = f"Data considerada para consulta/tratativa: {settings.processo.data_emissao_filtro}"
         logger.info(msg_data_tratativa)
 
-        app_consinco_operador = ConsincoOperadorDesktop(exe_path=settings.caminho.exe_erp_modulo_operador)
-        app_consinco_operador.acessar_tela_emissao_duplicatas_boletos()
-        app_consinco_operador.filtrar_todos_titulos_por_data(data_consulta=settings.processo.data_conciliacao)
-
-        logger.success(f"Processo {nome_rpa} executado com sucesso")
-
-    except Exception as error:
-        msg_erro = str(error)
-        execucao_com_erro = True
-        logger.error(f"Erro na execução do robô {nome_rpa}.\nErro: {error}")
-
-
-    finally:
-        app_consinco_operador.fechar_sistema() if app_consinco_operador else None
-
+        # Inicializa variaveis para envio de e-mail
         assunto_email = f"Resultado da execução do RPA - {nome_rpa}"
         corpo_email = str(
             "Resultado da execução do RPA\n"
             f"{msg_data_tratativa}\n\n\n"
         )
 
-        if execucao_com_erro:
-            assunto_email += " - ERRO"
-            corpo_email += str(
-                "\n\n\n"
-                "Houve um erro durante a execução do robô:\n"
-                f"{msg_erro}\n\n"
-                "Por favor, verifique os logs para mais detalhes."
-            )
-        else:
+        executar_cmds_manter_sessao_ativa()
+        fechar_sistemas_legados()
+
+        app_consinco_operador = ConsincoOperadorDesktop(exe_path=settings.caminho.exe_erp_modulo_operador)
+        app_consinco_operador.acessar_tela_emissao_duplicatas_boletos()
+        app_consinco_operador.consultar_titulos(data_consulta=settings.processo.data_emissao_filtro)
+        status_agendamento, msg_retorno = app_consinco_operador.agendar_envio_email_titulos(data_consulta=settings.processo.data_emissao_filtro)
+
+        if status_agendamento:
             assunto_email += " - SUCESSO"
+            corpo_email += f"{msg_retorno}\n"
+            logger.success(msg_retorno)
+        else:
+            assunto_email += " - ATENÇÃO"
+            corpo_email += f"{msg_retorno}\n"
+            logger.warning(msg_retorno)
+
+        logger.success(f"Processo {nome_rpa} executado com sucesso")
+
+    except Exception as error:
+        msg_erro = str(error)
+        assunto_email += " - ERRO"
+        corpo_email += f"{msg_erro}\n\n"
+
+        execucao_com_erro = True
+        logger.error(f"Erro na execução do robô {nome_rpa}.\nErro: {error}")
+
+    finally:
+        app_consinco_operador.fechar_sistema() if app_consinco_operador else None
 
         if settings.email.enviar_email:
             try:
